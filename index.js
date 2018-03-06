63a9982e9cefac48da6050b361e9884195731419
guitagun-3502 <37022012+guitagun-3502@users.noreply.github.com>
2018-03-06 20:58:55 +0300

    result

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 (function(){
 	var buttonsContainer = document.getElementsByClassName('buttons-container')[0];
+	var newsContainer = document.getElementsByClassName('news-container')[0];
 	buttonsContainer.addEventListener('click', onSourceClick);
-
 
 	var API_KEY = '6dc2f16f1c6245c8ac3b8a6815dc9044';
 	var sourcesUrl = 'https://newsapi.org/v2/sources?apiKey=' + API_KEY;
-	var topHeadlinesUrlTemplate = 'https://newsapi.org/v2/top-headlines?country=us&apiKey=' + API_KEY;
+	var topHeadlinesUrlTemplate = 'https://newsapi.org/v2/top-headlines?apiKey=' + API_KEY;
 
 
 	var sourcesXhr = new XMLHttpRequest();
@@ -36,9 +36,47 @@
 		button.setAttribute('data-source-id', source.id);
 		buttonsContainer.appendChild(button);
 	}
-
+	
+	
+	
+	
 	function onSourceClick(event){
+		newsContainer.innerText = '';
 		event.preventDefault();
+		var sourceId = event.target.getAttribute('data-source-id');
+		
+		if(sourceId){
+			getNews(sourceId);
+		}
+		
+	}
+	
+	function getNews(sourceId){
+		var url = topHeadlinesUrlTemplate + '&sources=' + sourceId;
+		var requestXhr = new XMLHttpRequest();
+		
+		requestXhr.open('GET', url);
+		requestXhr.send();
+		requestXhr.onreadystatechange = function(){
+			if(requestXhr.readyState != 4){
+				return;
+			}
+			
+			var news = JSON.parse(requestXhr.response).articles;
+			for(var i = 0,len = news.length; i < len; i++){
+				renderArtice(news[i]);
+			}
+		}
+		
+	}
+	
+	function renderArtice(article){
+		var elem = document.createElement('div');
+		var link = document.createElement('a');
+		link.innerText = article.url;
+		elem.innerText = article.title;
+		newsContainer.appendChild(elem);
+		newsContainer.appendChild(link);
 	}
 
 })();
